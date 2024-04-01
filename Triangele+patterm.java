6675760c6a28fe94d7b8a20b7de87ad2c075f90c
Yash Patil <102399703+yashdpatil@users.noreply.github.com>
2024-04-02 00:41:56 +0530

    Update Triangele+patterm.java

--- Triangele+patterm.java
+++ Triangele+patterm.java
@@ -24,3 +24,33 @@ public class Main
 }
 
 
+
+* * * * * * * 
+ * * * * * * 
+  * * * * * 
+   * * * * 
+    * * * 
+     * * 
+      * 
+
+	public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int n=7;
+		for(int i =1;i<=n;i++){
+		    for(int j =1;j<i;j++){
+		        System.out.print(" ");
+		    }
+		    for(int k = n;k>=i;k--){
+		        System.out.print("* ");
+		    }
+		    System.out.println();
+		}
+	}
+}
+
+
+
+	
+
