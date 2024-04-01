13b4a6f334cab15e97e207c8de89fc6e0b207a49
Yash Patil <102399703+yashdpatil@users.noreply.github.com>
2024-04-02 00:19:12 +0530

    Create Triangele+patterm.java

---
+++ Triangele+patterm.java
@@ -0,0 +1,26 @@
+       
+      * 
+     * * 
+    * * * 
+   * * * * 
+  * * * * * 
+ * * * * * * 
+
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+		for(int i=1;i<8;i++){
+		    for(int j=8;j>i;j--){
+		        System.out.print(" ");
+		    }
+		    for(int k=1;k<i;k++){
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+	}
+}
+
+
