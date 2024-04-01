40066fee5f8cb2ab6f9f8e2409379e3e840b23f6
Yash Patil <102399703+yashdpatil@users.noreply.github.com>
2024-04-02 00:52:15 +0530

    Create Pattern+char+print.java

---
+++ Pattern+char+print.java
@@ -0,0 +1,19 @@
+a
+ab
+abc
+abcd
+abcde
+
+  public class Main
+{
+	public static void main(String[] args) {
+		
+		for(char i='a';i<='e';i++){
+		    for(char j='a';j<=i;j++){
+		        System.out.print(j);
+		    }
+		   
+		    System.out.println();
+		}
+	}
+}
