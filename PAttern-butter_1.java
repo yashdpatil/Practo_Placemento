ee1a6a5ba874167a7ad254e2e5d7952f5691d5f0
Yash Patil <102399703+yashdpatil@users.noreply.github.com>
2024-04-02 00:23:13 +0530

    Update PAttern-butter_1.java

--- PAttern-butter_1.java
+++ PAttern-butter_1.java
@@ -30,3 +30,18 @@ public class Main
 		
 	}
 }
+
+
+
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+	for(int i=0;i<=5;i++){
+		    for(int j =0;j<5-i;j++){
+		        System.out.print("* ");
+		    }
+		    System.out.println();
+		}
+
