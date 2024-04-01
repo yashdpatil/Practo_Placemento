feeb14871f3ef43744d128b64688b104d4d7783a
Yash Patil <102399703+yashdpatil@users.noreply.github.com>
2024-04-02 00:18:03 +0530

    Create PAttern-butter_1.java

---
+++ PAttern-butter_1.java
@@ -0,0 +1,32 @@
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+
+  
+public class Main
+{
+	public static void main(String[] args) {
+	//	System.out.println("Hello World");
+	    for(int f = 1;f<=5;f++){
+	        for(int k =1;k<f;k++){
+	            System.out.print("* ");
+	        }
+	        System.out.println();
+	    }
+		for(int i=1;i<=5;i++){
+		    for(int j =5;j>=i;j--){
+		        System.out.print("* ");
+		    }
+		    System.out.println();
+		}
+		    
+		
+	}
+}
