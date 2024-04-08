e104bd6f5298e3f64771f06c10d60d6de5eb1ab7
Yash Patil <102399703+yashdpatil@users.noreply.github.com>
2024-04-08 12:53:18 +0530

    Update Palindromic_Traingle.java

--- Palindromic_Traingle.java
+++ Palindromic_Traingle.java
@@ -24,3 +24,23 @@ public class Main
   32123
  4321234
 543212345
+
+
+	public class Main {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int k =1;k<i;k++){
+                System.out.print(k);
+            }
+            for(int k =i;k>=1;k--){
+                System.out.print(k);
+            }
+              
+            System.out.println();
+        }
+    }
+}
